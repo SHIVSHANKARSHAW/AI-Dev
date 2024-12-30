4ee014227de3d5da67c50341a4d90f4ea2f7d9e1
Shiv <shivshankarshaw2003@gmail.com>
2024-12-30 22:37:51 +0530

    add login page and update routing; enhance documentation

--- frontend/src/Routes/AppRoutes.jsx
+++ frontend/src/Routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from '../pages/Login'
 const AppRoutes = () => {
   return (
     <BrowserRouter>
     <Routes>
-        <route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/login" element={<Login />} />
     </Routes>
     </BrowserRouter>
   )
